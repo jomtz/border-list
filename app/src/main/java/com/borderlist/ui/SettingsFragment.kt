365582a399057fc9f82eb0fcfe4c4da4c0e5724f
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2022-09-08 12:42:40 -0400

    Update Settings fragment

--- app/src/main/java/com/borderlist/ui/SettingsFragment.kt
+++ app/src/main/java/com/borderlist/ui/SettingsFragment.kt
@@ -1,43 +1,12 @@
 package com.borderlist.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.borderlist.databinding.FragmentSettingsBinding
-import com.borderlist.viewmodels.SettingsViewModel
+import androidx.preference.PreferenceFragmentCompat
+import com.borderlist.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
